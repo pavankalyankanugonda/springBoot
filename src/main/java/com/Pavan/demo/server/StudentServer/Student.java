ed1fe01d49cf4a1ad50e0292af11386926045a38
Pavan Kalyan <pavankalyankanugonda@gmail.com>
2026-07-14 14:16:56 +0530

    Add Post with student

---
+++ src/main/java/com/Pavan/demo/server/StudentServer/Student.java
@@ -0,0 +1,38 @@
+package com.Pavan.demo.server.StudentServer;
+
+public class Student {
+    int id;
+    String name;
+    int age;
+    String department;
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+}
--- src/main/java/com/Pavan/demo/server/StudentServer/StudentServer.java
+++ src/main/java/com/Pavan/demo/server/StudentServer/StudentServer.java
@@ -1,21 +1,28 @@
 package com.Pavan.demo.server.StudentServer;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentServer {
     //1. Store the Student
     @PostMapping("/create")
-    public String storeStudent(){
-        return """
-            id : 1
-            name : Pavan
-            Department : CSE
-            age : 25
-            """;
+    public String storeStudent(@RequestBody Student student){
+        int id = student.getId();
+        String name = student.getName();
+        int age = student.getAge();
+        String department = student.getDepartment();
+//        return """
+//            id : 1
+//            name : Pavan
+//            Department : CSE
+//            age : 25
+//            """;
+
+        return  "id: " + id + ", name: " + name + ", age: " + age + ", department: " + department;
     }
 
-    //2. Read the Studentr with id
+    //2. Read the Student with id
 
     //3. Update the student information
 
