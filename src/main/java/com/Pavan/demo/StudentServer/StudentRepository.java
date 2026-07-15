f7a7791d1cef40a130e646baff21e445893209d0
Pavan Kalyan <pavankalyankanugonda@gmail.com>
2026-07-15 11:35:07 +0530

    Add Spring Data Jpa

--- src/main/java/com/Pavan/demo/StudentServer/StudentRepository.java
+++ src/main/java/com/Pavan/demo/StudentServer/StudentRepository.java
@@ -1,11 +1,15 @@
 package com.Pavan.demo.StudentServer;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StudentRepository {
-    public Student save(Student student) {
-        System.out.println("StudentRepository save method");
-        return student;
+//public class StudentRepository {
+    public interface StudentRepository extends JpaRepository<Student, Integer> {
+
     }
-}
+    //public Student save(Student student) {
+//        System.out.println("StudentRepository save method");
+//        return student;
+    //}
+//}
